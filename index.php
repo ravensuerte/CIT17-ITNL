51edec2fa0609e98d76ba3c9b5951084aa364a8e
raven <opparavenjays@gmail.com>
2023-09-20 14:20:22 +0800

    first php file

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    echo"my first PHP";
+    echo"<br> yeahh its good";
+    echo"<br>";
+    echo"<br>";
+    echo"<br>";
+    echo"<br> yawaaaaaa<br>" ;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>press to continue</button>
+    <p>this is a <em>paragraphhhhhh<em></p>
+</body>
+</html>
