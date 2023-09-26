c070674ce21e6c9d13d4d54b2364e79ddfadd193
raven <opparavenjays@gmail.com>
2023-09-26 17:51:37 +0800

    modified index

--- index.php
+++ index.php
@@ -12,10 +12,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Document</title> 
+    <style>
+        h1{
+            text-align: center;
+        }
+    </style>
+    <h1>
+    <?php
+    echo "<br> this is part of the header";
+    ?>
+    </h1>
 </head>
 <body>
     <button>press to continue</button>
-    <p>this is a <em>paragraphhhhhh<em></p>
+    <p>this is a <em>paragraphhhhhh</em>
+        tatatatt
+        <?php
+        echo "<br> new line paragraph";
+        ?>
+    </p>
 </body>
 </html>
