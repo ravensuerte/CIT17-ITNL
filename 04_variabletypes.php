9f14ccf84bed6874d3091f99f81ec7f8d2a21956
M307users <m307user@m307.uc-bcf.edu.ph>
2023-09-27 13:20:19 +0800

    finished na!

--- 04_variabletypes.php
+++ 04_variabletypes.php
@@ -60,7 +60,61 @@
         print($literally);
         print("<br>");
 
+        
 
+    ?>
+    <?php
+    $channel =<<<_XML_
+    <channel>
+    <title>What's For Dinner<title>
+    <link>http://menu.example.com/<link>
+    <description>Choose what to eat tonight.</description>
+    </channel>
+    _XML_;
+    echo <<<END
+    This uses the "here document" syntax to output
+    multiple lines with variable interpolation. Note
+    that the here document terminator must appear on a
+    line with just a semicolon. no extra whitespace!
+    <br />
+    END;
+        print $channel;
+
+    ?>
+    <?php
+        $x = 4;
+        function assignx () {
+            $x = 0;
+print "\$x inside function is $x. ";
+}
+assignx();
+print "\$x outside of function is $x. ";
+
+// multiply a value by 10 and return it to the caller
+function multiply ($value) {
+    $value = $value * 10;
+    return $value;
+    }
+    $retval = multiply (10);
+    Print "Return value is $retval\n";
+
+    $somevar = 15;
+function addit() {
+GLOBAL $somevar;
+$somevar++;
+print "Somevar is $somevar";
+}
+addit();
+
+function keep_track() {
+    STATIC $count = 0;
+    $count++;
+    print $count;
+    print " ";
+}
+keep_track();
+keep_track();
+keep_track();
     ?>
 </body>
 </html>
